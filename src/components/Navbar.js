842d762a0bc282164c1b93a08cfa43ea5971a94c
Phương HCY <anhthich0k@gmail.com>
2022-05-18 21:34:43 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="h-23 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
+        <i className='fa fa-spotify mr-5'></i>
+        Spotify
+    </div>
+  )
+}
